112964fe8c156e411462c7ddbc25822e893ebf3a
ABDUL MATEEN BUTT <154554625+Butt43B@users.noreply.github.com>
2024-10-14 00:24:07 +0500

    Add files via upload

---
+++ gcdCalculation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int calculateGcd(int a,int b)
+{ 
+    while (b!=0)
+    {
+       int rem=a%b;
+       a=b;
+       b=rem;
+    }
+    return a;
+}
+void displayGcd()
+{
+    int num1;
+    cin>>num1;
+    int num2;
+    cin>>num2;
+   int result=calculateGcd(num1,num2);
+   cout<<result; 
+}
+int main()
+{
+    displayGcd();
+}
