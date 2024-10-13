3f339835fdfaf5cbebcdaa5f3f0747d6245f364a
ABDUL MATEEN BUTT <154554625+Butt43B@users.noreply.github.com>
2024-10-14 00:28:18 +0500

    Function for Table of Numbers

--- tablePrinting.cpp
+++ tablePrinting.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int calculationOfTable(int num)
 {
+    int num;
+    cin>>num;
     int result;
     for (int i = 1; i <= num; i++)
     {
-        
+        cout<<num*i;
     }
     
 
